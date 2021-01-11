a7041496e01508035a57b3ac8c34dfae76d057eb
Bart Huiskamp <bartolameus.h@gmail.com>
2021-01-11 17:44:50 +0100

    Bart's new user_input for the Calendar

---
+++ user_input_calendar.py
@@ -0,0 +1,30 @@
+
+input("Do you want Day, Time or Activity:  ")
+
+
+user_input_day = input ("Enter the day you want to add")
+user_input_time = input("Enter time slot")
+user_input_activity = input("fill in ")
+
+if they say day, they mean specific day. but if not they mean show all days
+Time (show time arrangment?)
+Activity?
+
+input day means specifick special activitie,
+time should be avilible time?
+
+
+
+#planning = adding_activeties
+#schedule = see_free_avilible
+#re-arrange = change planning
+#setup_main_planning =
+#this is to much think small
+
+
+
+#Ask the User for:
+
+#Day (If they say no day, assume they mean all days)
+#Time (But do not validate)
+#Activity
